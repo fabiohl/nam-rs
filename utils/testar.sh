1c993c0b4dccc2c7abbcfce6bb1e3ab74b5be4bc
Fabio Lima <fabio.henrique.lima.silva@gmail.com>
2026-04-18 23:53:27 -0300

    novo nam

--- utils/testar.sh
+++ utils/testar.sh
@@ -9,6 +9,7 @@ qpwgraph &
 
 echo "⚙️ Compilando o NAM-rs..."
 cargo build --release
+ls -lath target/release/nam-rs
 
 echo "🚀 Executando..."
 target/release/nam-rs --model tests/EVH-5150-Lite.nam --input-gain 2.0 --output-gain 5.0
